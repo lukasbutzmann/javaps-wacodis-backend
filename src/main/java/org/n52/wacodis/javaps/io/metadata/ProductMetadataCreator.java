5ad7741f02426f4b42f84bcb5b775a0a185045ad
SebaDro <sebastian.drost@hs-bochum.de>
2019-05-27 13:20:57 +0200

    Add JSON generator for product metadata outputs

---
+++ src/main/java/org/n52/wacodis/javaps/io/metadata/ProductMetadataCreator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.wacodis.javaps.io.metadata;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public interface ProductMetadataCreator<T> {
+
+    public ProductMetadata createProductMetadataBinding(T product);
+}
---
+++ src/main/java/org/n52/wacodis/javaps/io/metadata/SentinelProductMetadataCreator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.wacodis.javaps.io.metadata;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import org.esa.snap.core.datamodel.Product;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class SentinelProductMetadataCreator implements ProductMetadataCreator<Product> {
+
+    @Override
+    public ProductMetadata createProductMetadataBinding(Product product) {
+        ProductMetadata metadata = new ProductMetadata();
+        metadata.setTimeFrame(createTimeFrame(product));
+        return metadata;
+    }
+
+    private TimeFrame createTimeFrame(Product product) {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        df.setTimeZone(tz);
+
+        TimeFrame timeFrame = new TimeFrame();
+        timeFrame.setStartDate(df.format(product.getStartTime().getAsDate()));
+        timeFrame.setEndDate(df.format(product.getStartTime().getAsDate()));
+        return timeFrame;
+    }
+
+}
