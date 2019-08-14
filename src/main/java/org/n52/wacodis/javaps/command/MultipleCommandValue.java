586a8ea9c6b35311705607deaa96aa07ea5e0188
SebaDro <sebastian.drost@hs-bochum.de>
2019-08-14 11:10:18 +0200

    Introduce command value encapsulation

---
+++ src/main/java/org/n52/wacodis/javaps/command/MultipleCommandValue.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.wacodis.javaps.command;
+
+import java.util.List;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class MultipleCommandValue extends AbstractCommandValue<List<String>> {
+
+}
---
+++ src/main/java/org/n52/wacodis/javaps/command/SingleCommandValue.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.wacodis.javaps.command;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class SingleCommandValue extends AbstractCommandValue<String> {
+
+}
