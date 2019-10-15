4592f1dc4b9eaf14cf6393eedae0a8af0eaeda0d
SebaDro <sebastian.drost@hs-bochum.de>
2019-10-15 09:11:23 +0200

    Add more detailed exception handling

--- src/main/java/org/n52/wacodis/javaps/preprocessing/gpt/GptExecutor.java
+++ src/main/java/org/n52/wacodis/javaps/preprocessing/gpt/GptExecutor.java
@@ -7,6 +7,9 @@ package org.n52.wacodis.javaps.preprocessing.gpt;
 
 import java.util.Map;
 import org.esa.snap.core.gpf.main.GPT;
+import org.n52.wacodis.javaps.WacodisProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,14 +19,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class GptExecutor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GptExecutor.class);
+
     /**
      * Executes a GPF graph via GPT
      *
      * @param arguments {@link GptArguments} for executing the GPF graph via GPT
-     * @throws Exception
+     * @throws WacodisProcessingException
      */
-    public void executeGraph(GptArguments arguments) throws Exception {
-        GPT.run(this.prepareArguments(arguments));
+    public void executeGraph(GptArguments arguments) throws WacodisProcessingException {
+        LOGGER.info("Execute processing graph: {}", arguments.getGraphFile());
+        try {
+            GPT.run(this.prepareArguments(arguments));
+        } catch (Exception ex) {
+            LOGGER.error("Processing graph execution unexpectedly terminated.", ex);
+            throw new WacodisProcessingException("Error while executing processing graph.", ex);
+        }
     }
 
     /**
@@ -33,9 +44,10 @@ public class GptExecutor {
      * @return Array of String arguments
      */
     public String[] prepareArguments(GptArguments arguments) {
-        String[] args = new String[arguments.getParameters().size() + 1];
+        String[] args = new String[arguments.getParameters().size() + 2];
         int i = 0;
         args[i] = arguments.getGraphFile();
+        args[++i] = "-e";
 
         for (Map.Entry<String, String> entry : arguments.getParameters().entrySet()) {
             args[++i] = "-P" + entry.getKey() + "=" + entry.getValue();
