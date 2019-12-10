f2bca7ad535bf96d741e40298118987c9549e6f1
SebaDro <sebastian.drost@hs-bochum.de>
2019-12-10 11:46:05 +0100

    Adjust ShapeWriter for handling complex feature schemas

--- src/test/java/org/n52/wacodis/javaps/preprocessing/graph/ShapeWriterTest.java
+++ src/test/java/org/n52/wacodis/javaps/preprocessing/graph/ShapeWriterTest.java
@@ -5,17 +5,12 @@
  */
 package org.n52.wacodis.javaps.preprocessing.graph;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.io.FileUtils;
+import org.geotools.GML;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.geojson.feature.FeatureJSON;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -24,25 +19,38 @@ import org.n52.wacodis.javaps.WacodisProcessingException;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.referencing.FactoryException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- *
  * @author LukasButzmann
  */
 public class ShapeWriterTest {
-    
-     private static final String REFERENCE_DATA_FILE_NAME = "test-reference-data.json";
+
+    private static final String REFERENCE_DATA_FILE_NAME = "test-reference-data.xml";
+    private static final String TMP_SHAPE_DIR_PREFIX = "tmp-ref-dir";
 
     private SimpleFeatureCollection featureCollection;
     private ShapeWriter shapeWriter;
     private File tempTargetFile;
-    
+    private Path tmpShapeDir;
+
     @Before
-    public void init() throws IOException {
+    public void init() throws IOException, ParserConfigurationException, SAXException {
         InputStream input = this.getClass().getClassLoader().getResourceAsStream(REFERENCE_DATA_FILE_NAME);
-        this.featureCollection = (SimpleFeatureCollection) new FeatureJSON().readFeatureCollection(input);
-        
-        this.tempTargetFile = File.createTempFile("testFile", ".shp");
+        GML gml = new GML(GML.Version.WFS1_1);
+        this.featureCollection = gml.decodeFeatureCollection(input);
+        this.tmpShapeDir = Files.createTempDirectory(TMP_SHAPE_DIR_PREFIX);
+        this.tempTargetFile = File.createTempFile("testFile", ".shp", tmpShapeDir.toFile());
         this.shapeWriter = new ShapeWriter(tempTargetFile);
     }
 
@@ -53,8 +61,9 @@ public class ShapeWriterTest {
 
         Assert.assertFalse(resultCollection.isEmpty());
         Assert.assertTrue(resultFile.exists());
+        Assert.assertEquals("the_geom", resultCollection.getSchema().getGeometryDescriptor().getLocalName());
     }
-    
+
     private SimpleFeatureCollection shpToFeatureCollection(File result) throws MalformedURLException, IOException {
         Map<String, Object> map = new HashMap<>();
         map.put("url", result.toURI().toURL());
@@ -66,9 +75,9 @@ public class ShapeWriterTest {
 
         return collection;
     }
-    
+
     @After
     public void shutdown() throws IOException {
-        tempTargetFile.deleteOnExit();
+        FileUtils.deleteDirectory(this.tmpShapeDir.toFile());
     }
 }
