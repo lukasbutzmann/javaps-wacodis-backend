398e5dea7b840c3d8a599fff7770835a4653f79d
Arne Vogt <arne.vogt@hs-bochum.de>
2020-01-13 14:11:12 +0100

    add timestamp to container name to prevent naming conflicts

--- src/main/java/org/n52/wacodis/javaps/algorithms/execution/EoToolExecutor.java
+++ src/main/java/org/n52/wacodis/javaps/algorithms/execution/EoToolExecutor.java
@@ -56,12 +56,13 @@ public class EoToolExecutor {
         );
         DockerConfig dockerConfig = config.getDocker();
         CommandConfig cmdConfig = config.getCommand();
+        String containerName = dockerConfig.getContainer().trim() + "_" + System.currentTimeMillis(); //add timestamp to container name to prevent naming conflicts
 
         DockerController dockerController = initDockerController(dockerConfig);
         DockerRunCommandConfiguration dockerRunConfig = this.initRunConfiguration(cmdConfig, input);
         dockerRunConfig.addVolumeBinding(concatVolumeBinding(this.config.getWorkingDirectory(), dockerConfig.getWorkDir()));
 
-        DockerContainer dockerContainer = new DockerContainer(dockerConfig.getContainer(), dockerConfig.getImage());  //TODO use prefix in container name
+        DockerContainer dockerContainer = new DockerContainer(containerName, dockerConfig.getImage());
 
         LOGGER.info("executing tool inside docker container " 
                 + dockerContainer.getContainerName() 
