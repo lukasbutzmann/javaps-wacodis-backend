c8bb666712d4b81f6cad2d07b777663d93546264
SebaDro <sebastian.drost@hs-bochum.de>
2020-02-18 08:26:50 +0100

    Change masking data preprocessing to using graph based preprocessing

--- src/main/java/org/n52/wacodis/javaps/algorithms/SealingFactorAlgorithm.java
+++ src/main/java/org/n52/wacodis/javaps/algorithms/SealingFactorAlgorithm.java
@@ -5,42 +5,29 @@
  */
 package org.n52.wacodis.javaps.algorithms;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import org.apache.commons.io.FilenameUtils;
 import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Product;
 import org.geotools.data.simple.SimpleFeatureCollection;
-import org.n52.javaps.algorithm.annotation.Algorithm;
-import org.n52.javaps.algorithm.annotation.ComplexInput;
-import org.n52.javaps.algorithm.annotation.ComplexOutput;
-import org.n52.javaps.algorithm.annotation.Execute;
-import org.n52.javaps.algorithm.annotation.LiteralInput;
+import org.n52.javaps.algorithm.annotation.*;
 import org.n52.javaps.gt.io.data.binding.complex.GTVectorDataBinding;
 import org.n52.javaps.io.GenericFileData;
 import org.n52.wacodis.javaps.WacodisProcessingException;
 import org.n52.wacodis.javaps.command.AbstractCommandValue;
-import org.n52.wacodis.javaps.command.MultipleCommandValue;
-import org.n52.wacodis.javaps.command.SingleCommandValue;
 import org.n52.wacodis.javaps.io.data.binding.complex.GeotiffFileDataBinding;
 import org.n52.wacodis.javaps.io.data.binding.complex.ProductMetadataBinding;
 import org.n52.wacodis.javaps.io.http.SentinelFileDownloader;
 import org.n52.wacodis.javaps.io.metadata.ProductMetadata;
 import org.n52.wacodis.javaps.io.metadata.ProductMetadataCreator;
 import org.n52.wacodis.javaps.io.metadata.SentinelProductMetadataCreator;
-import org.n52.wacodis.javaps.preprocessing.GptPreprocessor;
-import org.n52.wacodis.javaps.preprocessing.InputDataPreprocessor;
-import org.n52.wacodis.javaps.preprocessing.ReferenceDataPreprocessor;
+import org.n52.wacodis.javaps.preprocessing.graph.*;
 import org.n52.wacodis.javaps.utils.GeometryUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 /**
  *
@@ -67,8 +54,8 @@ public class SealingFactorAlgorithm extends AbstractAlgorithm {
 
     private String opticalImagesSourceType;
     private String opticalImagesSource;
-    private String referenceDataType;
-    private SimpleFeatureCollection referenceData;
+    private String maskingDataType;
+    private SimpleFeatureCollection maskingData;
     private ProductMetadata productMetadata;
     private Product sentinelProduct;
 
@@ -103,8 +90,8 @@ public class SealingFactorAlgorithm extends AbstractAlgorithm {
             maxOccurs = 1,
             defaultValue = "MASK_VECTOR_DATA",
             allowedValues = {"MASK_VECTOR_DATA","MASK_RASTER_DATA"})
-    public void setReferenceDataType(String value) {
-        this.referenceDataType = value;
+    public void setMaskingDataType(String value) {
+        this.maskingDataType = value;
     }
 
     @ComplexInput(
@@ -115,8 +102,8 @@ public class SealingFactorAlgorithm extends AbstractAlgorithm {
             maxOccurs = 1,
             binding = GTVectorDataBinding.class
     )
-    public void setReferenceData(SimpleFeatureCollection value) {
-        this.referenceData = value;
+    public void setMaskingData(SimpleFeatureCollection value) {
+        this.maskingData = value;
     }
 
     @ComplexOutput(
@@ -140,7 +127,7 @@ public class SealingFactorAlgorithm extends AbstractAlgorithm {
         this.executeProcess();
 
         ProductMetadataCreator metadataCreator = new SentinelProductMetadataCreator();
-        this.productMetadata = metadataCreator.createProductMetadataBinding(this.sentinelProduct);
+        this.productMetadata = metadataCreator.createProductMetadata(this.sentinelProduct);
     }
 
     @Override
@@ -162,8 +149,8 @@ public class SealingFactorAlgorithm extends AbstractAlgorithm {
     public Map<String, AbstractCommandValue> createInputArgumentValues(String basePath) throws WacodisProcessingException {
         Map<String, AbstractCommandValue> inputArgumentValues = new HashMap();
 
-        inputArgumentValues.put("OPTICAL_IMAGES_SOURCES", this.createInputValue(basePath, this.preprocessOpticalImages()));
-        inputArgumentValues.put("MASKING_DATA", this.createInputValue(basePath, this.preprocessReferenceData()));
+        inputArgumentValues.put("OPTICAL_IMAGES_SOURCES", this.createInputValue(basePath, this.preprocessOpticalImages(), true));
+        inputArgumentValues.put("MASKING_DATA", this.createInputValue(basePath, this.preprocessReferenceData(), true));
         inputArgumentValues.put("RESULT_PATH", this.getResultPath(basePath));
 
         return inputArgumentValues;
@@ -185,10 +172,18 @@ public class SealingFactorAlgorithm extends AbstractAlgorithm {
     }
 
     private File preprocessReferenceData() throws WacodisProcessingException {
-        InputDataPreprocessor referencePreprocessor = new ReferenceDataPreprocessor(GeometryUtils.DEFAULT_INPUT_EPSG, this.getBackendConfig().getEpsg(), this.getNamingSuffix());
 
-        List<File> preprocessedReferenceData = referencePreprocessor.preprocess(this.referenceData, this.getBackendConfig().getWorkingDirectory());
-        return preprocessedReferenceData.get(0);
+        String fileIdentifier = (this.getNamingSuffix() != null) ? this.getNamingSuffix() : UUID.randomUUID().toString();
+        InputDataWriter shapeWriter = new ShapeWriter(new File(this.getBackendConfig().getWorkingDirectory(), "wacodis_maskingdata_" + fileIdentifier + ".shp"));
+
+        InputDataOperator reprojectingOperator = new ReprojectingOperator(GeometryUtils.DEFAULT_INPUT_EPSG);
+        List<InputDataOperator> referenceDataOperatorList = new ArrayList<>();
+        referenceDataOperatorList.add(reprojectingOperator);
+
+        PreprocessingExecutor referencePreprocessor = new PreprocessingExecutor(shapeWriter, referenceDataOperatorList);
+        File preprocessedReferenceData = referencePreprocessor.executeOperators(this.maskingData);
+
+        return preprocessedReferenceData;
     }
 
 }
