e670e3304558664d73bd6d30d7f6f53cb0d54ead
lukasbutzmann <lukasbutzmann@yahoo.de>
2020-05-14 09:08:40 +0200

    add WCSTestAlgorithm.java to download images from WebCoverageService

---
+++ src/main/java/org/n52/wacodis/javaps/algorithms/WCSTestAlgorithm.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.wacodis.javaps.algorithms;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.geotools.geometry.jts.JTS;
+import org.locationtech.jts.geom.CoordinateList;
+import org.n52.javaps.algorithm.annotation.Algorithm;
+import org.n52.javaps.algorithm.annotation.Execute;
+import org.n52.javaps.algorithm.annotation.LiteralInput;
+import org.n52.javaps.algorithm.annotation.LiteralOutput;
+import org.n52.wacodis.javaps.GeometryParseException;
+import org.n52.wacodis.javaps.WacodisProcessingException;
+import org.n52.wacodis.javaps.io.http.WCSFileDownloader;
+import org.n52.wacodis.javaps.utils.GeometryUtils;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
+import org.openide.util.Exceptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Lukas Butzmann
+ */
+@Algorithm(
+        identifier = "de.hsbo.wacodis.wcs_test_algorithm",
+        title = "WCS Test Algorithm",
+        abstrakt = "Downloads Geotiffs in an Area of Interests and aggrigate them.",
+        version = "1.0.0",
+        storeSupported = true,
+        statusSupported = true)
+public class WCSTestAlgorithm{
+    
+    private static final double MAX_RASTER_RANGE_WCS_RESPONSE = 2000;
+    
+    //TODO: Parameter aus URL
+    private static final String SUBSETTINGCRS = "EPSG:4326";
+    private static final double SCALEFACTOR = 0.1;
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(WCSTestAlgorithm.class);
+    
+    private String wcsUrl;
+    private String areaOfInterest;
+    private List<String> wcsRequestList;
+    String[][] requests;
+    
+    @Autowired
+    private WCSFileDownloader wcsDownloader;
+    
+    @LiteralInput(
+            identifier = "WCS_DATA",
+            title = "WCS",
+            abstrakt = "WCS",
+            minOccurs = 1,
+            maxOccurs = 1
+    )
+    public void setReferenceData(String value) {
+        this.wcsUrl = value;
+    }
+    
+    @LiteralInput(
+            identifier = "AREA_OF_INTEREST",
+            title = "Area of interest",
+            abstrakt = "Area of interest of the optical image in GeoJSON-Format e.g. [7.1234, 52.1234, 7.9876, 52.9876]. [0,0,0,0] uses the entire area of the image.",
+            minOccurs = 1,
+            maxOccurs = 1
+    )
+    public void setAreaOfInterest(String value) {
+        this.areaOfInterest = value;
+    }
+    
+    //TODO: Output merged Geotiff
+    @LiteralOutput(
+            identifier = "PRODUCT"
+    )
+    public String getOutput() {
+        return Arrays.toString(requests);
+    }
+    
+    @Execute
+    public void execute()throws WacodisProcessingException, GeometryParseException{
+        
+        //BBOX to CoordinateList
+        CoordinateList coordListBbox = GeometryUtils.getCoordinatesOfBbox(areaOfInterest);
+        
+        //EPSG Code to CRS
+        CoordinateReferenceSystem crs = GeometryUtils.decodeCRS(SUBSETTINGCRS);
+        
+        //Extend of images
+        double width = 0;
+        double height = 0;
+        try{
+            width = JTS.orthodromicDistance(coordListBbox.get(0), coordListBbox.get(2), crs);
+            height = JTS.orthodromicDistance(coordListBbox.get(0), coordListBbox.get(1), crs);
+        }catch (TransformException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        
+        //Number of images
+        int numberOfImagesWidht =  (int) (Math.ceil(width/(MAX_RASTER_RANGE_WCS_RESPONSE/SCALEFACTOR)));
+        int numberOfImagesHeight =  (int) (Math.ceil(height/(MAX_RASTER_RANGE_WCS_RESPONSE/SCALEFACTOR)));
+        //int numberOfImages = numberOfImagesWidht*numberOfImagesHeight;
+        
+        double minX = coordListBbox.get(0).x;
+        double maxX = coordListBbox.get(2).x;
+        double minY = coordListBbox.get(0).y;
+        double maxY = coordListBbox.get(1).y;
+
+        double imageWidht = (maxX - minX) / numberOfImagesWidht;
+        double imageHeight = (maxY - minY) / numberOfImagesHeight;
+
+        requests = new String[numberOfImagesWidht][numberOfImagesHeight];
+        this.wcsRequestList = new ArrayList();
+        //Coordinates of picturers
+        for (int i = 0; numberOfImagesWidht > i; i++) {
+            double xMin = minX + i * (imageWidht);
+            double xMax = minX + (i + 1) * (imageWidht);
+            for (int j = 0; numberOfImagesHeight > j; j++) {
+                double yMin = minY + j * imageHeight;
+                double yMax = minY + (j + 1) * imageHeight;
+                wcsRequestList.add(wcsRequestBuilder(xMin, xMax, yMin, yMax));
+            }
+        }
+
+        List<File> wcsFileList = new ArrayList();
+        
+        this.wcsRequestList.forEach(wcsr -> {
+            try {
+                // Download wcs data
+                File wcsFile = wcsDownloader.downloadWCSFile(wcsr, wcsr.hashCode());
+                wcsFileList.add(wcsFile);
+            } catch (IOException ex) {
+                LOGGER.error("Error while retrieving WCS file: {}", wcsr, ex);
+            }
+        });
+    }
+    
+    public String wcsRequestBuilder(double xMin, double xMax, double yMin, double yMax){
+        String subset = "&SUBSET=x("+xMin+","+xMax+")&SUBSET=y("+yMin+","+yMax+")";
+        String wcsrequest = wcsUrl+subset;                
+        return wcsrequest;
+    }    
+    
+}
---
+++ src/main/java/org/n52/wacodis/javaps/io/http/WCSFileDownloader.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.wacodis.javaps.io.http;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
+import org.n52.wacodis.javaps.configuration.WacodisBackendConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Lukas Butzmann
+ */
+@Component
+public class WCSFileDownloader {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WCSFileDownloader.class);
+
+    private final Map<String, File> productToFileCache = new HashMap<>();
+    
+    private WacodisBackendConfig config;
+
+    private RestTemplate basicHttpService;
+
+    @Autowired
+    public void setConfig(WacodisBackendConfig config) {
+        this.config = config;
+    }
+
+    @Autowired
+    public void setWCSService(@Qualifier("basicHttpService")RestTemplate basicHttpService) {
+        this.basicHttpService = basicHttpService;
+    }
+    
+    public File downloadWCSFile(String url, int hashcode) throws IOException {
+
+        LOG.info("Downloading WCS product: {}", url);
+
+        // Optional Accept header
+        RequestCallback callback = (ClientHttpRequest request) -> {
+            request.getHeaders()
+                    .setAccept(Arrays.asList(MediaType.valueOf("image/tiff")));
+        };
+
+        ResponseExtractor<File> responseExtractor = (ClientHttpResponse response) -> {
+            String fileName = response.getHeaders().getContentDisposition().getFilename();
+            File imageFile = new File(config.getWorkingDirectory() + "/ELEVATION/"+ hashcode + "_" + fileName);
+            FileUtils.copyInputStreamToFile(response.getBody(), imageFile);
+            return imageFile;
+        };
+
+        try {
+            File imageFile = basicHttpService.execute(url, HttpMethod.GET, callback, responseExtractor);
+
+            LOG.info("Downloading of WCS product successful: {}", url);
+
+            synchronized (WCSFileDownloader.this) {
+                productToFileCache.put(url, imageFile);
+            }
+
+            return imageFile;
+        } catch (HttpStatusCodeException ex) {
+            LOG.error("GET request for WCS file {} returned status code: {}.",
+                    ex.getStatusCode());
+            throw new IOException(ex);
+        } catch (RestClientException ex) {
+            LOG.error("Unexpected client error while requesting WCS file ", ex.getMessage());
+            throw new IOException(ex);
+        }
+    }
+}
