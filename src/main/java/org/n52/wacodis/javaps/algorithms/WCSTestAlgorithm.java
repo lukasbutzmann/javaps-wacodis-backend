d92d60b0f675ba66083718231b02c1ed10db933a
lukasbutzmann <lukasbutzmann@yahoo.de>
2020-05-26 11:20:56 +0200

    add dockertool to merge multiple geotiff files

--- src/main/java/org/n52/wacodis/javaps/algorithms/WCSTestAlgorithm.java
+++ src/main/java/org/n52/wacodis/javaps/algorithms/WCSTestAlgorithm.java
@@ -9,7 +9,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.io.FilenameUtils;
 import org.geotools.geometry.jts.JTS;
 import org.locationtech.jts.geom.CoordinateList;
 import org.n52.javaps.algorithm.annotation.Algorithm;
@@ -18,7 +22,14 @@ import org.n52.javaps.algorithm.annotation.LiteralInput;
 import org.n52.javaps.algorithm.annotation.LiteralOutput;
 import org.n52.wacodis.javaps.GeometryParseException;
 import org.n52.wacodis.javaps.WacodisProcessingException;
+import org.n52.wacodis.javaps.algorithms.execution.EoToolExecutor;
+import org.n52.wacodis.javaps.command.AbstractCommandValue;
+import org.n52.wacodis.javaps.command.MultipleCommandValue;
+import org.n52.wacodis.javaps.command.ProcessResult;
+import org.n52.wacodis.javaps.command.SingleCommandValue;
 import org.n52.wacodis.javaps.configuration.WacodisBackendConfig;
+import org.n52.wacodis.javaps.configuration.tools.ToolConfig;
+import org.n52.wacodis.javaps.configuration.tools.ToolConfigParser;
 import org.n52.wacodis.javaps.io.http.WCSFileDownloader;
 import org.n52.wacodis.javaps.utils.GeometryUtils;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -44,6 +55,8 @@ public class WCSTestAlgorithm{
     private static final double MAX_RASTER_RANGE_WCS_RESPONSE = 2000;
     private static final double SCALEFACTOR = 0.1;
     private static final String SUBSETTINGCRS = "EPSG:4326";
+    private static final String GDAL_CONFIG = "gdal-merge-geotiffs.yml";
+    private static final String GDAL_RESULT_POSTFIX = "_merged";
     
     private static final Logger LOGGER = LoggerFactory.getLogger(WCSTestAlgorithm.class);
     
@@ -58,7 +71,15 @@ public class WCSTestAlgorithm{
     public void setConfig(WacodisBackendConfig config) {
         this.config = config;
     }
+    
+    @Autowired
+    private ToolConfigParser toolConfigParser;
 
+    @Autowired
+    private EoToolExecutor eoToolExecutor;
+    
+    private String namingSuffix = "_" + System.currentTimeMillis();
+    
     @Autowired
     private WCSFileDownloader wcsDownloader;
     
@@ -149,6 +170,8 @@ public class WCSTestAlgorithm{
                 LOGGER.error("Error while retrieving WCS file: {}", wcsr, ex);
             }
         });
+        
+        executeGdalMergeGeotiffs(wcsFileList);
     }
     
     public String wcsRequestBuilder(double xMin, double xMax, double yMin, double yMax){
@@ -159,4 +182,101 @@ public class WCSTestAlgorithm{
         return wcsrequest;
     }
     
+    protected void executeDockerTool(Map<String, AbstractCommandValue> inputArgumentValues, ToolConfig toolConfig) throws WacodisProcessingException {
+        ProcessResult result;
+        try {
+            result = eoToolExecutor.executeTool(inputArgumentValues, toolConfig);
+        } catch (Exception ex) {
+            String message = "Error while executing docker process";
+            LOGGER.debug(message, ex);
+            throw new WacodisProcessingException(message, ex);
+        }
+        if (result.getResultCode() != 0) { //tool returns Result Code 0 if finished successfully
+            throw new WacodisProcessingException(String.format("EO tool (container: %s) exited with non-zero result code (%s)." +
+                            " Cause: %s. Consult tool specific documentation for details",
+                    toolConfig.getDocker().getContainer(),
+                    result.getResultCode(),
+                    result.getOutputMessage()));
+        }
+        LOGGER.info("EO tool docker process finished "
+                + "executing with result code: {}", result.getResultCode());
+        LOGGER.debug(result.getOutputMessage());
+
+    }
+    
+    protected ToolConfig getToolConfig(String toolConfigPath) throws WacodisProcessingException {
+        ToolConfig toolConfig = null;
+        try {
+            toolConfig = toolConfigParser.parse(toolConfigPath);
+            toolConfig.getDocker().setContainer(toolConfig.getDocker().getContainer().trim() + this.namingSuffix); //add unique suffix to container name to prevent naming conflicts
+            return toolConfig;
+        } catch (IOException ex) {
+            String message = "Error while reading tool configuration";
+            LOGGER.debug(message, ex);
+            throw new WacodisProcessingException(message, ex);
+        }
+    }
+    
+    protected File executeGdalMergeGeotiffs(List<File> files) throws WacodisProcessingException {
+        ToolConfig toolConfig = this.getToolConfig(this.getToolConfigPath(GDAL_CONFIG));
+        String resultName = FilenameUtils.concat(config.getWorkingDirectory(),
+                "WCS"
+                + GDAL_RESULT_POSTFIX
+                + "."
+                + FilenameUtils.getExtension(files.get(0).getName()));
+        File outFile = new File(resultName);
+        Map<String, AbstractCommandValue> inputArgumentValues = this.createGdalInputArgumentValues(files, toolConfig.getDocker().getWorkDir(), outFile);
+
+        this.executeDockerTool(inputArgumentValues, toolConfig);
+
+        return outFile;
+    }
+
+    protected Map<String, AbstractCommandValue> createGdalInputArgumentValues(List<File> inFiles, String basePath, File outFile) {
+        Map<String, AbstractCommandValue> inputArgumentValues = new HashMap();
+
+        inputArgumentValues.put("INPUTS", this.createInputValue(basePath, inFiles, true));
+        inputArgumentValues.put("OUTPUT", this.createInputValue(basePath, outFile, true));
+
+        return inputArgumentValues;
+    }
+    
+        /**
+     * Creates input argument values from a {@link List} of input data
+     * {@link File} objects
+     *
+     * @param basePath  base path where to read the input data from
+     * @param inputData input data as {@link List} of {@link File} objects
+     * @return {@link AbstractCommandValue} that encapsulates a {@link List} of
+     * input data file paths
+     * @throws WacodisProcessingException
+     */
+    protected AbstractCommandValue createInputValue(String basePath, List<File> inputData, boolean forUnix) {
+        MultipleCommandValue value = new MultipleCommandValue(
+                inputData.stream()
+                        .map(sF -> {
+                            String path = FilenameUtils.concat(basePath, sF.getName());
+                            return forUnix ? FilenameUtils.separatorsToUnix(path) : path;
+                        })
+                        .collect(Collectors.toList()));
+        return value;
+    }
+
+     /**
+     * Creates input argument values from an input data {@link File}
+     *
+     * @param basePath  base path where to read the input data from
+     * @param inputData input data as {@link File}
+     * @return {@link AbstractCommandValue} that encapsulates an input data file
+     * path
+     * @throws WacodisProcessingException
+     */
+    protected AbstractCommandValue createInputValue(String basePath, File inputData, boolean forUnix) {
+        String path = FilenameUtils.concat(basePath, inputData.getName());
+        return forUnix ? new SingleCommandValue(FilenameUtils.separatorsToUnix(path)) : new SingleCommandValue(path);
+    }
+    
+    protected String getToolConfigPath(String toolConfigName) {
+        return this.config.getToolConfigDirectory() + "/" + toolConfigName;
+    }
 }
